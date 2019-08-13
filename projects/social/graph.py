fa4bba543d06def56e9250c5752864032d729343
AmarachiOE <ao.ejiawoko@gmail.com>
2019-08-13 14:56:42 -0700

    getAllSocialPaths()

---
+++ projects/social/graph.py
@@ -0,0 +1,300 @@
+"""
+Simple graph implementation
+"""
+# using visited = set() and visited.add(vertex) adds to the set but sorts the values (doesn't maintain order)
+from util import Stack, Queue  # These may come in handy
+
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        """
+        Add a vertex to the graph.
+        """
+        # TODO
+        if not vertex in self.vertices:
+            self.vertices[vertex] = set()
+        else:
+            print("Warning: Vertex already exists.")
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph. Check that vertices are present in graph.
+        """
+        # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            # self.vertices[v2].add(v1) #  for if bi-directional edge
+        else:
+            print("Warning: One or more of the vertices do not exist")
+
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        # TODO
+        # Create empty queue, enqueue the starting vertex - FIFO
+        # Create a set to store the visited vertices
+        # While queue is not empty...
+            # Dequeue the first vertex
+            # If that vertex has not been visited...
+                # Mark visited
+                 # Add all of its neighbors (node's siblings/share same parent/those nodes on the same level) to the back of queue
+        q = Queue()
+        q.enqueue(starting_vertex)
+        visited = []
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                # print("now visited: ", v)
+                visited.append(v)
+
+                for next_vert in self.vertices[v]:
+                    q.enqueue(next_vert)
+
+        # print("BFT: ", visited)
+        return visited
+
+        # while q.size() > 0:
+        #     v = q.queue[0]
+        #     for vertex in self.vertices[v]:
+        #         if vertex not in visited:
+        #             q.enqueue(vertex)
+        #             visited.add(vertex)
+        #     q.dequeue()
+
+    def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
+        # TODO
+        # Create an empty stack and push the starting vertex ID
+        # Create a Set to store visited vertices
+        # While the stack is not empty...
+            # Pop the first vertex
+            # If that vertex has not been visited...
+                # Mark it as visited...
+                # Then add all of its neighbors to the top/end of the stack
+
+        s = Stack()
+        s.push(starting_vertex)
+        visited = []
+
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                # print("now visited: ", v)
+                visited.append(v)
+
+                for next_vert in self.vertices[v]:
+                    s.push(next_vert)
+
+        # print("DFT: ", visited)
+        return visited
+
+    def dft_recursive(self, starting_vertex, visited = None): # must pass visited as arg so share among recursions and not reset each round
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        This should be done using recursion.
+        """
+        if visited is None: # or use visited=[] as arg in place of this if statement
+            visited = []
+
+        visited.append(starting_vertex)
+
+        for vertex in self.vertices[starting_vertex]:
+            if vertex not in visited:
+                self.dft_recursive(vertex, visited) # add visited as arg so we can share the same list of visited and not reset it to [] for each recursion
+        
+        # print("DFT Recursive: ", visited)
+        return visited
+
+    def bfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing the shortest path from
+        starting_vertex to destination_vertex in
+        breath-first order.
+        """
+        #### ORIGINAL TAKE
+        # q = Queue()
+        # q.enqueue(starting_vertex)
+        # visited = []
+        # while q.size() > 0:
+        #     v = q.dequeue()
+        #     if v not in visited:
+        #         visited.append(v)
+
+        #         for next_vert in self.vertices[v]:
+        #             if next_vert not in visited:
+        #                 q.enqueue(next_vert)
+        #             # q.enqueue(next_vert)
+        #             if next_vert == destination_vertex:
+        #                 visited.append(next_vert)
+        #                 return visited
+
+        # return visited
+
+        #### FROM CLASS
+        q = Queue()
+
+        q.enqueue([starting_vertex])
+
+        visited = []
+
+        while q.size() > 0:
+            path = q.dequeue() # path = returned first item in queue
+            v = path[-1] # current vert = last item in path
+
+            if v not in visited:
+                if v == destination_vertex:
+                    return path
+
+                visited.append(v)
+
+                for next_vert in self.vertices[v]:
+                    """ 
+                    a = 5
+                    b = a
+                    a += 1
+                    print(a, b) --> 6, 5
+                    new_path is the same data as path, but we want an actual copy, not just point to the same reference, so we use list(path)
+                    since we want to explore different paths
+
+                    """
+                    new_path = list(path)
+                    new_path.append(next_vert)
+                    q.enqueue(new_path)
+           
+
+        return None
+
+    def dfs(self, starting_vertex, destination_vertex):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+        """
+        #### ORIGINAL TAKE
+        # s = Stack()
+        # s.push(starting_vertex)
+        # visited = []
+
+        # while s.size() > 0:
+        #     v = s.pop()
+        #     if v not in visited:
+        #         visited.append(v)
+
+        #         for next_vert in self.vertices[v]:
+        #             s.push(next_vert)
+        #             if next_vert == destination_vertex:
+        #                 visited.append(next_vert)
+        #                 return visited
+
+        # return visited
+
+        #### FROM CLASS
+        s = Stack()
+
+        s.push([starting_vertex])
+
+        visited = []
+
+        while s.size() > 0:
+            path = s.pop() # path = returned first item in queue
+            v = path[-1] # last item in path
+
+            if v not in visited:
+                if v == destination_vertex:
+                    return path
+                visited.append(v)
+
+                for next_vert in self.vertices[v]:
+                    new_path = list(path)
+                    new_path.append(next_vert)
+                    s.push(new_path)
+           
+
+        return None
+
+
+if __name__ == '__main__':
+    graph = Graph()  # Instantiate your graph
+    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
+
+    '''
+    Should print:
+        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    '''
+    print("Graph Verts: ", graph.vertices)
+
+    '''
+    Valid DFT paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    print("DFT: ", graph.dft(1))
+
+    '''
+    Valid BFT paths:
+        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 7, 6
+        1, 2, 3, 4, 6, 7, 5
+        1, 2, 3, 4, 6, 5, 7
+        1, 2, 3, 4, 7, 6, 5
+        1, 2, 3, 4, 7, 5, 6
+        1, 2, 4, 3, 5, 6, 7
+        1, 2, 4, 3, 5, 7, 6
+        1, 2, 4, 3, 6, 7, 5
+        1, 2, 4, 3, 6, 5, 7
+        1, 2, 4, 3, 7, 6, 5
+        1, 2, 4, 3, 7, 5, 6
+    '''
+    print("BFT: ", graph.bft(1))
+
+    '''
+    Valid DFT recursive paths:
+        1, 2, 3, 5, 4, 6, 7
+        1, 2, 3, 5, 4, 7, 6
+        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 6, 3, 5, 7
+    '''
+    print("DFT Recursive: ", graph.dft_recursive(1))
+
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    print("BFS: ", graph.bfs(1, 6))
+
+    '''
+    Valid DFS paths:
+        [1, 2, 4, 6]
+        [1, 2, 4, 7, 6]
+    '''
+    print("DFS: ", graph.dfs(1, 6))
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,14 +1,22 @@
+import random
+import math
+from util import Queue
+from graph import Graph
 
 
 class User:
     def __init__(self, name):
         self.name = name
 
+
 class SocialGraph:
     def __init__(self):
         self.lastID = 0
         self.users = {}
         self.friendships = {}
+
+    def __repr__(self):
+        return f"Friendships: {self.friendships}"
 
     def addFriendship(self, userID, friendID):
         """
@@ -44,11 +52,28 @@ class SocialGraph:
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
+
+        # TODO:
 
         # Add users
+        for i in range(0, numUsers):
+            self.addUser(f"Brian{i}")
 
-        # Create friendships
+        # Create Friendships
+        possibleFriendships = []
+        for userID in self.users:
+            for friendID in range(userID + 1, self.lastID + 1):
+                possibleFriendships.append(
+                    (userID, friendID))  # appending a tuple of current user and all possible friends being users after them in list (userID + 1)
+
+        # changes the position of items in a list
+        random.shuffle(possibleFriendships)
+        # random.sample(possibleFriendships, (numUsers * avgFriendships) // 2)
+
+        for i in range(0, math.floor(numUsers * avgFriendships / 2)):
+            # remember possibleFriendships is a list of tuples of friends [(val1, val2), (val1, val2), (val1, val2)]
+            friendship = possibleFriendships[i]
+            self.addFriendship(friendship[0], friendship[1])
 
     def getAllSocialPaths(self, userID):
         """
@@ -59,14 +84,63 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+
+        # TODO: !!!! IMPLEMENT ME
+
+        if not self.friendships[userID]:
+            return "User has no friends"
+
+        # use bft from Graph
+        # g = Graph()
+        # g.vertices = self.friendships
+
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+
+        q = Queue()
+        path = [userID]
+        q.enqueue(path)
+        while q.size() > 0:
+            # print("Current Queue: ", q.queue)
+            path = q.dequeue()  # remove/return next path block in line
+
+            # Get last item in path block
+            # This is the end friend
+            # The path block holds the connections bw the starting user (userID) to this end friend
+            # Path format ex:
+            # [start, end] or
+            # [start, next,... next, end]
+            # So we want to add the end item/friend as a key in dictionary, with the full path block/array as the value
+            # in dict: for user to get to this item/friend/key: the path of user's friends
+            v = path[-1]
+
+            if v not in visited:
+
+                if v is not userID:  # starting user doesn't need to be in dictionary
+                    visited[v] = path
+
+                for friend in self.friendships[v]:
+                    new_path = list(path)  # or = path[:]
+                    new_path.append(friend)  # adding friend to path
+                    # adds path as a block [friend, friend, friend], so queue is a list of lists
+                    q.enqueue(new_path)
+
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
-    print(sg.friendships)
+    print("Friendships: ", sg.friendships)
     connections = sg.getAllSocialPaths(1)
-    print(connections)
+    print("Connections: ", connections)
+
+""" In Command Line: 
+$ cd into social folder
+$ python3
+>>> from social import *
+>>> sg = SocialGraph()
+>>> sg.populateGraph(10, 2)
+>>> print(sg.friendships)
+>>> print(sg.getAllSocialPaths(1))
+"""
+
